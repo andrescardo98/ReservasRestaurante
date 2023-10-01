44561929e68e5d05dad385be19d3fa99c58ff88b
Andrés Cardona <andres.cardo98@gmail.com>
2023-10-01 18:46:54 -0500

    Crear DAO SQLSERVER y Excepciones - Reservas Restaurante (actualización de nombres

--- src/main/java/co/edu/uco/reservasrestaurante/crosscutting/exception/concrete/DataReservasRestauranteException.java
+++ src/main/java/co/edu/uco/reservasrestaurante/crosscutting/exception/concrete/DataReservasRestauranteException.java
@@ -2,8 +2,9 @@ package co.edu.uco.reservasrestaurante.crosscutting.exception.concrete;
 
 import co.edu.uco.reservasrestaurante.crosscutting.exception.ReservasRestauranteException;
 
-public class ServiceTiendaOnlineException extends ReservasRestauranteException{
+public final class DataReservasRestauranteException extends ReservasRestauranteException{
 
 	private static final long serialVersionUID = 2765757041071477495L;
+	
 
 }
--- src/main/java/co/edu/uco/reservasrestaurante/crosscutting/exception/concrete/ServiceReservasRestauranteException.java
+++ src/main/java/co/edu/uco/reservasrestaurante/crosscutting/exception/concrete/ServiceReservasRestauranteException.java
@@ -2,9 +2,8 @@ package co.edu.uco.reservasrestaurante.crosscutting.exception.concrete;
 
 import co.edu.uco.reservasrestaurante.crosscutting.exception.ReservasRestauranteException;
 
-public final class DataTiendaOnlineException extends ReservasRestauranteException{
+public class ServiceReservasRestauranteException extends ReservasRestauranteException{
 
 	private static final long serialVersionUID = 2765757041071477495L;
-	
 
 }
